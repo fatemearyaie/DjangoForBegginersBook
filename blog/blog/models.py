c4fae9c0df9ef8f80e355f169901f77fc3d7cc63
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-27 09:24:10 +0330

    push all files such as migrations or pycache

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.urls import reverse
+from django.conf import settings
+
 # Create your models here.
 class Posts(models.Model):
-    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     body = models.TextField()
 
--- blog/django_project/settings.py
+++ blog/django_project/settings.py
@@ -129,3 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'postlist'
 LOGOUT_REDIRECT_URL = 'postlist'
+
+
+AUTH_USER_MODEL = 'accounts.CostumUser'
