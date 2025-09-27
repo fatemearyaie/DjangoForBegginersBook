cb30797d2d6a4392f32f2d2d97ac71019a368689
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-27 09:23:37 +0330

    change admin panel settings for costumuser model

--- blog/accounts/admin.py
+++ blog/accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CostumUserChangeForm, CostumUserCreationForm
+from .models import CostumUser
 
 # Register your models here.
+class CostumUserAdmin(UserAdmin):
+    form = CostumUserChangeForm
+    model = CostumUser
+    list_display = [
+        'email',
+        'username',
+        'age',
+        'is_staff'
+    ]
+
+admin.site.register(CostumUser, CostumUserAdmin)
