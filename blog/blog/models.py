e9f3c2419e985f345b9a7c9858354b6adadcd7c0
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-27 10:27:46 +0330

    update files

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,14 +1,10 @@
 from django.db import models
-from django.urls import reverse
 from django.conf import settings
-
 # Create your models here.
-class Posts(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
-    def get_absolute_url(self):
-        return reverse("postdetail", kwargs={"pk": self.pk})
+        return self.title[:50]
--- blog/blog/tests.py
+++ blog/blog/tests.py
@@ -1,21 +1,3 @@
 from django.test import TestCase
-from django.contrib.auth import get_user_model
-from .models import Posts
+
 # Create your tests here.
-class BlogTests(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = get_user_model().objects.create_user(
-        username="testuser", email="test@email.com", password="secret"
-        )
-        cls.post = Posts.objects.create(
-        title="A good title",
-        body="Nice body content",
-        author=cls.user,
-        )
-    def test_post_model(self):
-        self.assertEqual(self.post.title, "A good title")
-        self.assertEqual(self.post.body, "Nice body content")
-        self.assertEqual(self.post.author.username, "testuser")
-        self.assertEqual(str(self.post), "A good title")
-        self.assertEqual(self.post.get_absolute_url(), "/post/1/")
