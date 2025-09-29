ca8c96f9a0f36b8c0a6c12d574d6ba35ad8f3dff
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-29 12:10:12 +0330

    add CRUD and detail urls

--- newspaper/articles/urls.py
+++ newspaper/articles/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import CreateArticle
+from .views import CreateArticle, ListArticle, UpdateArticle, DeleteArticle, DetailArticle
 
 urlpatterns = [
-    path('createarticle/', CreateArticle.as_view(), name='create'),
+    path('createarticle/', CreateArticle.as_view(), name='articlecreate'),
+    path('', ListArticle.as_view(), name='articlelist'),
+    path('editarticle/<int:pk>/', UpdateArticle.as_view(), name='articleedit'),
+    path('deletearticle/<int:pk>/', DeleteArticle.as_view(), name='articledelete'),
+    path('detailarticle/<int:pk>/', DetailArticle.as_view(), name='articledetail',)
+
 ]
