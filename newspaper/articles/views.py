d620bce3f1ae3bc0e1bcb3e7c448b10a10c54c87
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-30 11:52:08 +0330

    add userpassesstestmixin and test_func def

--- newspaper/articles/views.py
+++ newspaper/articles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DeleteView, UpdateView , DetailView
 from .models import Article
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 class CreateArticle(LoginRequiredMixin, CreateView):
@@ -19,15 +19,24 @@ class ListArticle(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'articles/articles_list.html'
 
-class UpdateArticle(LoginRequiredMixin, UpdateView):
+class UpdateArticle(LoginRequiredMixin,UserPassesTestMixin , UpdateView):
     model = Article
     template_name = 'articles/edit_article.html'
     fields = ['title', 'body']
 
-class DeleteArticle(LoginRequiredMixin, DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class DeleteArticle(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
     model = Article
     template_name = 'articles/delete_article.html'
     success_url = reverse_lazy('list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+    
 
 class DetailArticle(LoginRequiredMixin, DetailView):
     model = Article
