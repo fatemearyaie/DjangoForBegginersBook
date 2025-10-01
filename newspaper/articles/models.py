8c68b2e3329bd99d4bb2448b8e216f8f862767c0
fatemearyaie <fatemearyaie9@gmail.com>
2025-10-01 11:38:49 +0330

    add comment model

--- newspaper/articles/models.py
+++ newspaper/articles/models.py
@@ -14,4 +14,16 @@ class Article(models.Model):
     
     def get_absolute_url(self):
         return reverse("model_detail", kwargs={"pk": self.pk})
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=255)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment[:50]
+    
+    def get_absolute_url(self):
+        return reverse("article_list")
     
